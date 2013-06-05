be8bde2ea2e0aad8c76f44a5e9a96763d6cbd8c6
Art Willow Hundiak <ahundiak@zayso.org>
2013-06-05 16:28:19 -0500

    schedule import

--- src/Cerad/Bundle/AppBundle/Command/ScheduleCommand.php
+++ src/Cerad/Bundle/AppBundle/Command/ScheduleCommand.php
@@ -29,126 +29,33 @@ class ScheduleCommand extends ContainerAwareCommand
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $file = 'Schedule20130509.xls';
+        $file = 'Schedule20130601.xls';
         $datax = $this->getParameter('datax');
+        
+        $project = $this->getService('cerad_tourn.project');
         
         $params = array
         (
-            'truncate' => true,
+            'truncate'  => true,
+            'persist'   => true,
             'output'    => 'Post', // Scan, Excel
-            'sport'     => 'Soccer',
-            'season'    => 'SP2013',
-            'domain'    => 'AYSOA5B',
-            'domainSub' => 'Games',
+            'project'   => $project,
+            
+          //'sport'     => 'Soccer',
+          //'season'    => 'SP2013',
+          //'domain'    => 'AYSOA5B',
+          //'domainSub' => 'Games',
             
             'defaultGameStatus' => 'Normal',
             'inputFileName'     => $datax . $file,
           //'worksheetName'     => 'Schedule',
         );
-        $import = $this->getService('cerad_tourn.schedule.import');
+        $import = $this->getService('cerad_app.schedule.import');
         
         $results = $import->importFile($params);
 
       //$this->testLoadLesSchedule();
       //$this->testLoadArbiterSchedule();
-    }
-    protected function testLoadLesSchedule()
-    {
-        $loader = $this->getService('cerad_arbiter.schedule.tourn.les.load');
-        
-        $datax = $this->getParameter('datax');
-        
-        $items = $loader->load($datax . '/classic/ScheduleLes20130415.csv');
-        
-        echo sprintf("Games: %d\n",count($items));
-        
-        $saver = $this->getService('cerad_arbiter.schedule.tourn.arbiter.save');
-        
-        $saver->save($datax . '/classic/ScheduleArbiter20130415.csv',$items);
-    }
-    protected function testLoadArbiterSchedule()
-    {
-        $loader = $this->getService('cerad_arbiter.schedule.tourn.arbiter.load');
-        
-        $datax = $this->getParameter('datax');
-        
-        $items = $loader->load($datax . '/classic/Schedule20130418.xls');
-        
-        echo sprintf("Games: %d\n",count($items));
-        
-        $saver = $this->getService('cerad_arbiter.schedule.tourn.referee.save');
-        
-        $saver->save($datax . '/classic/ScheduleReferee20130418.csv',$items);
-    }
-    /* ===========================================================================
-     * Old stuff
-     */
-    
-    protected function test6()
-    {
-      //$importArbiter = new ImportArbiterSchedule();
-      //$arbiterGames = $importArbiter->import('../datax/ClassicSchedule20120420.csv');
-        
-      //echo 'Arbiter Game Count: ' . count($arbiterGames) . "\n";
-       
-        $lesLoad = new LoadLesSchedule();
-        $lesGames  = $lesLoad->load('../datax/KicksLes20121014.csv');
-        
-        echo 'Les     Game Count: ' . count($lesGames) . "\n";
-        
-        $lesSave = new SaveArbiterSchedule();
-        $lesSave->save('../datax/KicksArbiter20121014.csv',$lesGames);
-        
-        return;
-        
-        $compare = new CompareSchedules();
-        $compare->compare($arbiterGames,$lesGames);
-    }
-    protected function testPositions()
-    {
-        $arbiterLoad = new LoadArbiterSchedule();
-        $arbiterGames = $arbiterLoad->load('../datax/KicksArbiter20121021.csv');
-        
-        echo 'Arbiter Game Count: ' . count($arbiterGames) . "\n";
-     
-        $arbSave = new SaveRefereeSchedule();
-        $arbSave->save('../datax/KicksReferees20121021.csv',$arbiterGames);
-        
-        return;
-    }
-    protected function testCompare()
-    {
-        $arbiterLoad = new LoadArbiterSchedule();
-        $arbiterGames = $arbiterLoad->load('../datax/KicksArbiter20121021.csv');
-        
-        echo 'Arbiter Game Count: ' . count($arbiterGames) . "\n";
-       
-        $lesLoad = new LoadLesSchedule();
-        $lesGames  = $lesLoad->load('../datax/KicksLes20121017.csv');
-        
-        echo 'Les     Game Count: ' . count($lesGames) . "\n";
-        
-        $compare = new CompareSchedules();
-        $compare->compare($arbiterGames,$lesGames);
-    }
-     protected function testLoadArbiter()
-    {
-        $arbiterLoad = $this->getContainer()->get('zayso_arbiter.schedule.load');
-        $arbiterGames = $arbiterLoad->load('../datax/KicksArbiterSchedule20121024.xls');
-        
-        echo 'Arbiter Game Count: ' . count($arbiterGames) . "\n";
-        
-        $gameNums = array_keys($arbiterGames);
-        
-        $game = $arbiterGames[$gameNums[4]];
-        echo sprintf("Game %s %s %s %s\n",$game->getGameNum(),$game->getSite(),$game->getHomeTeam(),$game->getCR());
-        
-        return;
-        
-        $arbiterGames = $arbiterLoad->load('../datax/KicksArbiter20121021.csv');
-        
-        echo 'Arbiter Game Count: ' . count($arbiterGames) . "\n";
-        
     }
 }
 ?>
