ab9ee5689f5a21b373bd54bd5aba19f914558d5c
Art Willow Hundiak <ahundiak@zayso.org>
2013-06-17 09:29:48 -0500

    tag - v1.2013.06

--- app/autoload.php
+++ app/autoload.php
@@ -8,7 +8,7 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-$loader->add('Cerad',   __DIR__  . '/../../cerad/src');
+$loader->add('Cerad',   __DIR__  . '/../../cerad201306v/src');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
